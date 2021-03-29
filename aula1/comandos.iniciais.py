cc556576abdb6371d35305e3b0a2153c3b65d3a7
otaviov <amandavv32@gmail.com>
2021-03-29 00:27:15 -0300

    Update comandos.iniciais.py

--- aula1/comandos.iniciais.py
+++ aula1/comandos.iniciais.py
@@ -1,6 +1,8 @@
 #Escrevendo Olá mundo na tela e me apresentando.
 print ('Olá, mundo!')
-print (' Eu me chamo Otávio :)')
+#perguntando ao usuário o nome: 
+nome = input('Como você se chama?')
+print ('Olá', nome, 'Prazer em conhece-lo(ª). Eu me chamo Otávio')
 
 #Variáveis 
 
@@ -9,5 +11,6 @@ n2 = int(input('Digite um outro valor: '))
 #Somando os dois números digitados pelo usuário, (input). 
 soma = (n1+n2)
 #Imprimido a soma.
-print('A soma dos números digitados é: ', soma)
+#print('A soma dos números digitados é: ', soma) ou
+print('A soma entre', n1, 'e', n2, 'vale:', soma)
 
