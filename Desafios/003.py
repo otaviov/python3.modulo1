4b685c1cb3d4d9da85685c493dabca0c67378d21
otaviov <amandavv32@gmail.com>
2021-03-29 00:40:39 -0300

    Create 003.py

---
+++ Desafios/003.py
@@ -0,0 +1,9 @@
+'''
+    Crie um programa que leia dois números e 
+    mostre a soma entre eles.
+'''
+
+n1 = int(input('Digite o primeiro número:'))
+n2 = int(input('Digite o segundo número:'))
+soma = n1 + n2
+print ('A soma dos números {} e {} vale: {}'.format(n1, n2, soma))
