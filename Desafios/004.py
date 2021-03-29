6bda4007c4f183b1a47f92151f5b864a16bb3fc8
otaviov <amandavv32@gmail.com>
2021-03-29 17:25:17 -0300

    Update 004.py

--- Desafios/004.py
+++ Desafios/004.py
@@ -4,3 +4,13 @@
     primitivo e todos as informações 
     possíveis sobre ele.
 '''
+
+#criando uma variável para guardar valor do usuário.
+valor = input('Digite algo: ')
+print('O tipo primitivo desse valor é:', type(valor))
+print('Só tem espaços?', valor.isspace())
+print('É um número?', valor.isnumeric())
+print('É alfabético?', valor.isalpha())
+print('Está em maiúsculas?',valor.isupper())
+print('Está em minúsculas?', valor.islower())
+print('Está capitalizada?', valor.istitle())
