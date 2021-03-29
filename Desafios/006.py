32610a5594d7a0b7415a8992dbebc18035109be7
otaviov <amandavv32@gmail.com>
2021-03-29 20:02:08 -0300

    resolvendo 006 e 007

--- Desafios/006.py
+++ Desafios/006.py
@@ -3,4 +3,4 @@ Crie um algoritmo que leia um número e mostre
 o seu dobro. triplo e raiz quadrada.
 '''
 n = int(input('Digite um número: '))
-print()
+print(' O dobro de {} é {}\n O tripo é {}\n e a raiz quadrada {:.2f}'.format(n, n*2, n*3, n**(1/2)))
