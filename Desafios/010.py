509d8ac5fac9f9228dca9d9203fdcdafc30bf348
otaviov <amandavv32@gmail.com>
2021-03-30 01:07:22 -0300

    010

--- Desafios/010.py
+++ Desafios/010.py
@@ -5,3 +5,7 @@ dólares ea pode comprar.
 considere US$1,00 = 3,27.
 '''
 
+print('{:*^20}'.format(' CONVERTENDO REAIS EM DÓLAR '))
+real = float(input('Quanto você tem na carteira? '))
+dolar = real / 3,27
+print('Com R${} você pode comprar U${:.2f}'.format(real, dolar))
