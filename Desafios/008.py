e57803d62473a3c34cecba52bcedf110b1326989
otaviov <amandavv32@gmail.com>
2021-03-29 21:58:35 -0300

    respondendo 008

--- Desafios/008.py
+++ Desafios/008.py
@@ -3,3 +3,7 @@ Escreva um programa que leia um valor em
 metros e o exiba convertido em centímetros 
 e milímitros.
 '''
+valor = float(input('Digite um valor em metros:'))
+cent = valor*100
+mili = valor*1000 
+print('o valor em metros é {}m\nem centímetros {:.0f}cm\ne em milímetros {:.0f}cm'.format(valor, cent, mili))
