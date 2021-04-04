879f7a56d8e33112034fd21adafe8e0b2b90b18b
otaviov <amandavv32@gmail.com>
2021-04-04 18:53:48 -0300

    atualizando metodos

--- Desafios/012.py
+++ Desafios/012.py
@@ -3,7 +3,15 @@ Faça um algoritmo que leia o preço de um
 produto e mostre seu novo preço, com 5% de
 desconto.
 '''
+
+#Primeiro método para resolução.
+'''
 preco = float(input('Digite o preço: '))
 porcen = preco*5/100
-desc = porcen-preco
-print('Seu produto de: R${:.2f} com desconto de {:.2f} tem o valor final de: R${:.2f}'.formart(preco,porcen,desc))
+desc = preco-porcen
+print('Seu produto de R${:.2f} com desconto: R${:.2f} fica com valor final: R${:.2f}'.format(preco, porcen, desc))
+'''
+
+#Segundo Método para resolução:
+preco = float(input('Digite o preço do produto: '))
+print('Seu produto de R${:.2f} com desconto de 5% R${:.2f} produto com desconto final {:.2f}'.format(preco, preco*5/100, preco-preco*5/100))
